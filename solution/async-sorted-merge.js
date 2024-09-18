1457961957ae5b093b91d359d367ac334a655a6c
adnan96ahmed <adnan_a96@hotmail.com>
2024-09-18 17:53:31 -0400

    finished solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,30 @@
-"use strict";
+'use strict';
+const MinHeap = require('heap'); // Using a min-heap to sort logs by date
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
+  const heap = new MinHeap((a, b) => a.log.date - b.log.date);
+  await Promise.all(
+    logSources.map(async (source, index) => {
+      const log = await source.popAsync();
+      if (log) {
+        heap.push({ log, index });
+      }
+    })
+  );
+
+  while (!heap.empty()) {
+    const { log, index } = heap.pop();
+    printer.print(log);
+    const nextLog = await logSources[index].popAsync();
+    if (nextLog) {
+      heap.push({ log: nextLog, index });
+    }
+  }
+
+  printer.done();
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    resolve(console.log('Async sort complete.'));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,26 @@
-"use strict";
+'use strict';
+const MinHeap = require('heap'); // Using a min-heap to sort logs by date
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const heap = new MinHeap((a, b) => a.log.date - b.log.date);
+  logSources.forEach((source, index) => {
+    const log = source.pop();
+    if (log) {
+      heap.push({ log, index });
+    }
+  });
+
+  while (!heap.empty()) {
+    const { log, index } = heap.pop();
+    printer.print(log);
+    const nextLog = logSources[index].pop();
+    if (nextLog) {
+      heap.push({ log: nextLog, index });
+    }
+  }
+
+  printer.done();
+  return console.log('Sync sort complete.');
 };
